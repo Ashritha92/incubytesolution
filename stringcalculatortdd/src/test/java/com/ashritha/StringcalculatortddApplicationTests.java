b610e40bb40d30a1985870b7bd11e64c1e2b6fca
nuthi <nuthi@DESKTOP-V4E14G8>
2021-10-17 18:26:31 +0530

    methods for less than 2 numbers is added in test

--- stringcalculatortdd/src/test/java/com/ashritha/StringcalculatortddApplicationTests.java
+++ stringcalculatortdd/src/test/java/com/ashritha/StringcalculatortddApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ashritha;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,6 +12,15 @@ class StringcalculatortddApplicationTests {
 	@Test
     public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
         StringCalculator.add("1,2,3");
+    }
+	@Test
+    public final void when2NumbersAreUsedThenNoExceptionIsThrown() {
+        StringCalculator.add("1,2");
+
+    }
+    @Test
+    public final void whenNonNumberIsUsedThenExceptionIsThrown() {
+        StringCalculator.add("1,X");
     }
 
 }
